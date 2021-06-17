c454e85f82b929c9a8e76c004fa8722149edcb54
Michel Joaquim <66667178+MIchelJoaquim@users.noreply.github.com>
2021-06-17 03:00:05 +0100

    feat: protect routes

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,12 +1,10 @@
-import { randomInt } from 'crypto';
-import { Request, Response, Router } from 'express';
+
+import { Router } from 'express';
 
 import { login, signup, tokenVerify } from '../controllers/auth';
 import { readAllHistoric } from '../controllers/historic';
 import { createMarket, readAllMarket } from '../controllers/market';
-import consultantModel from '../models/consultant.model';
-import historicModel from '../models/historic.model';
-import marketModel from '../models/market.model';
+import validateJWT from '../middleware/validateJwt';
 import { createHistoric } from './../controllers/historic';
 
 const router: Router = Router();
@@ -17,12 +15,12 @@ router.get('/auth/verify', tokenVerify);
 
 router.post('/auth/register', signup);
 
-router.post('/market', createMarket);
+router.post('/market', validateJWT, createMarket);
 
-router.get('/market', readAllMarket);
+router.get('/market', validateJWT, readAllMarket);
 
-router.get('/historic', readAllHistoric);
+router.get('/historic', validateJWT, readAllHistoric);
 
-router.post('/historic', createHistoric);
+router.post('/historic', validateJWT, createHistoric);
 
 export default router;
